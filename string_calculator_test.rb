78435c2810bfb1900ce188c32f6df374a6d5c57a
Bonnie Simon <bonnie.developer@gmail.com>
2025-01-09 17:11:57 +0530

    test: addition of single number

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -9,4 +9,8 @@ class StringCalculatorTest < Minitest::Test
   def test_ensure_addition_of_empty_string_returns_zero
     assert_equal 0, @calculator.add("")
   end
+
+  def test_addition_of_a_single_number
+    assert_equal 1, @calculator.add("1")
+  end
 end
