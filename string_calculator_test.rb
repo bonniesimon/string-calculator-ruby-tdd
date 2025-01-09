964a64f8c6d3395cdae9908c7ac9436423818cac
Bonnie Simon <bonnie.developer@gmail.com>
2025-01-09 16:54:29 +0530

    test: addition of empty string

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -5,4 +5,8 @@ class StringCalculatorTest < Minitest::Test
   def setup
     @calculator = StringCalculator.new
   end
+
+  def test_ensure_addition_of_empty_string_returns_zero
+    assert_equal 0, @calculator.add("")
+  end
 end
