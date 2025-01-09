c66f00c5e3f04ec4fad35f4485cd39dc88681e2f
Bonnie Simon <bonnie.developer@gmail.com>
2025-01-09 18:02:37 +0530

    refac: rename to parse_numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,14 +4,14 @@ class StringCalculator
   def add(input)
     return 0 if input.empty?
 
-    numbers = get_numbers_from_input(input)
+    numbers = parse_numbers(input)
 
     return numbers.reduce(&:+)
   end
 
   private
 
-    def get_numbers_from_input(input)
+    def parse_numbers(input)
       input
         .split(Regexp.union(delimiters))
         .map(&:to_i)
